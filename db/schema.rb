b4a1d8f6dde77aa12605fa72c8142970a5b09cc4
Setoju <setoju48@gmail.com>
2025-11-27 23:59:21 +0200

    fix: Change order items field name

---
+++ db/schema.rb
@@ -0,0 +1,93 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2025_11_25_134315) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+  end
+
+  create_table "deliveries", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.string "delivery_type"
+    t.datetime "delivery_time"
+    t.string "city"
+    t.string "address"
+    t.string "payment_type"
+    t.string "delivery_notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "postal_code"
+    t.string "phone"
+    t.string "delivery_status", default: "pending"
+    t.datetime "delivered_at"
+    t.index ["order_id"], name: "index_deliveries_on_order_id"
+  end
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string "name"
+    t.string "size"
+    t.decimal "price", precision: 10, scale: 2
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+    t.boolean "available", default: true
+    t.string "image_url"
+    t.integer "quantity"
+    t.index ["category_id"], name: "index_menu_items_on_category_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "menu_item_id", null: false
+    t.decimal "price", precision: 10, scale: 2
+    t.integer "total_quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "subtotal", precision: 10, scale: 2
+    t.index ["menu_item_id"], name: "index_order_items_on_menu_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.decimal "total_amount"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "notes"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email", null: false
+    t.string "phone"
+    t.string "password_digest"
+    t.string "role", default: "customer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "deliveries", "orders"
+  add_foreign_key "menu_items", "categories"
+  add_foreign_key "order_items", "menu_items"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "users"
+end
